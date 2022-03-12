36724c512b208d2f60d8f9bf2405b73191ddbb18
hugosenari <hugosenari@gmail.com>
2022-03-12 04:35:30 -0300

    chore: re index notmuch

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''wrapper for the notmuch mail library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."notmuch-master".url = "path:./master";
+  inputs."notmuch-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."notmuch-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
